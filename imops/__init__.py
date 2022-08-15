16ab75fb01c33ce80a6b8572d3a461f71850f256
Max <maxs987@gmail.com>
2022-08-15 17:52:59 +0300

    added crop and pad operations

--- imops/__init__.py
+++ imops/__init__.py
@@ -2,3 +2,5 @@ from .__version__ import __version__
 from .interp1d import interp1d
 from .radon import inverse_radon, radon
 from .zoom import _zoom, zoom, zoom_to_shape
+from .pad import pad, pad_to_shape, restore_crop
+from .crop import crop_to_box, crop_to_shape
---
+++ tests/test_crop.py
@@ -0,0 +1,44 @@
+import pytest
+import numpy as np
+
+from imops import crop_to_box
+
+assert_eq = np.testing.assert_array_equal
+
+
+def test_shape():
+    for _ in range(100):
+        shape = np.random.randint(10, 50, size=2)
+        box_shape = np.random.randint(1, 50, size=2)
+        box_center = [np.random.randint(s) for s in shape]
+        start = box_center - box_shape // 2
+
+        x = np.empty(shape)
+        box = np.stack([start, start + box_shape])
+
+        assert (crop_to_box(x, box, padding_values=0).shape == box_shape).all()
+
+
+def test_axes():
+    x = np.random.randint(0, 100, (3, 20, 23))
+
+    assert_eq(x[:, 1:15, 2:14], crop_to_box(x, np.array([[1, 2], [15, 14]]), axis=[1, 2]))
+
+    assert_eq(
+        x[:, 1:, 2:],
+        crop_to_box(x, np.array([[1, 2], [40, 33]]), padding_values=0, axis=(1, 2))[:, :19, :21]
+    )
+
+    assert_eq(
+        x[:, :15, :14],
+        crop_to_box(x, np.array([[-10, -5], [15, 14]]), padding_values=0, axis=(1, 2))[:, 10:, 5:]
+    )
+
+
+def test_raises():
+    x = np.empty((3, 20, 23))
+    with pytest.raises(ValueError):
+        crop_to_box(x, np.array([[1], [40]]), axis=(1, 2))
+
+    with pytest.raises(ValueError):
+        crop_to_box(x, np.array([[-1], [1]]), axis=(1, 2))
