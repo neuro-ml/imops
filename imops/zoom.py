8d02be10d0171a38ef775d0121adfb122ecccf3c
Max <maxs987@gmail.com>
2022-08-19 23:22:14 +0300

    simplified zoom

--- imops/zoom.py
+++ imops/zoom.py
@@ -67,7 +67,7 @@ def zoom_to_shape(
 
 def _zoom(
     input: np.ndarray,
-    zoom: Union[float, Sequence[float]],
+    zoom: Sequence[float],
     output: np.ndarray = None,
     order: int = 1,
     mode: str = 'constant',
@@ -85,32 +85,24 @@ def _zoom(
 
     See `https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.zoom.html`
     """
-    if input.dtype not in (np.float32, np.float64):
-        raise ValueError('Only fp32 and fp64 dtypes are allowed for zoom.')
-
     ndim = input.ndim
 
-    if ndim > 3:
+    if (
+        input.dtype not in (np.float32, np.float64) or ndim > 3 or
+        output is not None or order != 1 or mode != 'constant' or grid_mode):
+        warn(
+            'Fast interpolation is only supported for ndim<=3, dtype=float32 or float64, output=None, '
+            'order=1, mode=constant, grid_mode=False. Falling back to scipy\'s implementation', UserWarning
+        )
+
         return scipy_zoom(
             input, zoom, output=output, order=order, mode=mode, cval=cval, prefilter=prefilter, grid_mode=grid_mode
         )
 
-    if output is not None:
-        raise NotImplementedError('Only output=None is implemented for ndim <= 3.')
-    if order != 1:
-        raise NotImplementedError('Only 1-st order interpolation is implemented for ndim <= 3.')
-    if mode != 'constant':
-        raise NotImplementedError('Only constant mode is implemented for ndim <= 3.')
-    if grid_mode is not False:
-        raise NotImplementedError('Only grid_mode=False is implemented for ndim <= 3.')
+    if num_threads < 0:
+        max_threads = os.cpu_count()
+        num_threads = max_threads + num_threads + 1
 
-    if isinstance(zoom, (Sequence, np.ndarray)):
-        if len(zoom) != ndim:
-            raise RuntimeError(f'Zoom argument {zoom} must be number or have length equal to the input rank {ndim}.')
-    else:
-        zoom = [zoom for _ in range(ndim)]
-
-    num_threads = num_threads if num_threads != -1 else os.cpu_count()
     n_dummy = 3 - ndim
 
     if n_dummy:
--- tests/test_zoom.py
+++ tests/test_zoom.py
@@ -1,23 +1,16 @@
 from itertools import permutations
 
 import numpy as np
-import pytest
-from dpipe.im import zoom as dpipe_zoom, zoom_to_shape as dpipe_zoom_to_shape
+from dpipe.im import zoom_to_shape as dpipe_zoom_to_shape
 from numpy.testing import assert_allclose as allclose
 from scipy.ndimage import zoom as scipy_zoom
 
-from imops import _zoom, zoom, zoom_to_shape
+from imops import zoom, zoom_to_shape
 from imops.utils import get_c_contiguous_permutaion, inverse_permutation
 
 
 # [:-1, :-1, :-1] below is used because of the strange scipy.ndimage.zoom behaviour at the edge
 # https://github.com/scipy/scipy/issues/4922
-SEED = 1337
-
-
-@pytest.fixture(params=[(_zoom, scipy_zoom), (zoom, dpipe_zoom)])
-def zooms(request):
-    return request.param
 
 
 def test_zoom_to_shape():
@@ -30,39 +23,37 @@ def test_zoom_to_shape():
         without_borders = np.index_exp[:-1, :-1, :-1][: inp.ndim]
 
         # rtol=1e-6 as there is still some inconsistency
+        result = zoom_to_shape(inp, new_shape, axis=axis)
+        assert result.shape == tuple(new_shape)
+
         allclose(
-            zoom_to_shape(inp, new_shape, axis=axis)[without_borders],
+            result[without_borders],
             dpipe_zoom_to_shape(inp, new_shape, order=1, axis=axis)[without_borders],
             rtol=1e-6,
             err_msg=f'{i, shape, new_shape}',
         )
 
 
-def test_identity(zooms):
-    test_zoom, _ = zooms
+def test_identity():
     for i in range(16):
         shape = np.random.randint(2, 128, size=np.random.randint(1, 4))
         inp = np.random.randn(*shape)
 
-        allclose(inp, test_zoom(inp, np.ones(inp.ndim)), err_msg=f'{i, shape}')
+        allclose(inp, zoom(inp, 1), err_msg=f'{i, shape}')
 
 
-def test_dtype(zooms):
-    test_zoom, target_zoom = zooms
+def test_dtype():
     for dtype in (np.float32, np.float64):
         for i in range(4):
             shape = np.random.randint(2, 128, size=np.random.randint(1, 4))
             inp = (10 * np.random.randn(*shape)).astype(dtype)
             inp_copy = np.copy(inp)
-            scale = np.random.uniform(0.5, 2, size=1 if np.random.binomial(1, 0.5) else inp.ndim)
-            if len(scale) == 1:
-                scale = scale[0]
+            scale = np.random.uniform(0.5, 2, size=inp.ndim)
 
             without_borders = np.index_exp[:-1, :-1, :-1][: inp.ndim]
-            axis = {'axis': np.arange(len(shape))} if test_zoom == zoom else {}
 
-            out = test_zoom(inp, scale, **axis)
-            desired_out = target_zoom(inp, scale, order=1, **axis)
+            out = zoom(inp, scale)
+            desired_out = scipy_zoom(inp, scale, order=1)
 
             # FIXME: fix inconsistency
             # rtol=1e-6 as there is still some inconsistency
@@ -73,20 +64,14 @@ def test_dtype(zooms):
             assert inp.dtype == inp_copy.dtype == dtype, f'{i, inp.dtype, inp_copy.dtype, dtype}'
 
 
-def test_scale_types(zooms):
-    test_zoom, target_zoom = zooms
+def test_scale_types():
     scales = [2, 2.0, (2, 2, 2), [2, 2, 2], np.array([2, 2, 2])]
 
     inp = np.random.randn(64, 64, 64)
-    axis = {'axis': np.arange(len(inp.shape))} if test_zoom == zoom else {}
-
     prev = None
 
     for scale in scales:
-        out = test_zoom(inp, scale, **axis)
-        desired_out = target_zoom(inp, scale, order=1, **axis)
-
-        allclose(out, desired_out, err_msg=f'{scale}')
+        out = zoom(inp, scale)
 
         if prev is not None:
             allclose(prev, out, err_msg=f'{scale}')
@@ -94,19 +79,17 @@ def test_scale_types(zooms):
         prev = out
 
 
-def test_contiguity_awareness(zooms):
-    test_zoom, target_zoom = zooms
+def test_contiguity_awareness():
     for i in range(2):
         for j in range(2):
             inp = np.random.randn(*(64,) * (3 - i))
             scale = np.random.uniform(0.5, 2, size=inp.ndim)
-            axis = {'axis': np.arange(len(inp.shape))} if test_zoom == zoom else {}
 
             # start = time()
-            test_zoom(inp, scale, **axis)
+            zoom(inp, scale)
             # runtime = time() - start
 
-            desired_out = target_zoom(inp, scale, order=1, **axis)
+            desired_out = scipy_zoom(inp, scale, order=1)
             without_borders = np.index_exp[:-1, :-1, :-1][: inp.ndim]
 
             for permutation in permutations(range(inp.ndim)):
@@ -114,7 +97,7 @@ def test_contiguity_awareness(zooms):
                 permuted = np.transpose(inp, permutation)
 
                 # start_permuted = time()
-                out_permuted = test_zoom(permuted, scale[np.array(permutation)], **axis)
+                out_permuted = zoom(permuted, scale[np.array(permutation)])
                 # runtime_permuted = time() - start_permuted
 
                 allclose(
@@ -130,41 +113,37 @@ def test_contiguity_awareness(zooms):
                 ), f"Didn't find permutation for {i, j, permutation}"
 
 
-def test_thin(zooms):
-    test_zoom, target_zoom = zooms
+def test_thin():
     for i in range(3):
         for j in range(16):
             shape = [1 if k < i else np.random.randint(2, 128) for k in range(3)]
             inp = np.random.randn(*shape)
             scale = np.random.uniform(0.5, 2, size=3)
-            axis = {'axis': np.arange(len(inp.shape))} if test_zoom == zoom else {}
 
             without_borders = np.index_exp[
-                : None if shape[0] == 1 else -1, : None if shape[1] == 1 else -1, : None if shape[2] == 1 else -1
-            ]
+                              : None if shape[0] == 1 else -1, : None if shape[1] == 1 else -1,
+                              : None if shape[2] == 1 else -1
+                              ]
             allclose(
-                test_zoom(inp, scale, **axis)[without_borders],
-                target_zoom(inp, scale, order=1, **axis)[without_borders],
+                zoom(inp, scale)[without_borders],
+                scipy_zoom(inp, scale, order=1)[without_borders],
                 err_msg=f'{i, j, shape, scale}',
             )
 
 
-def test_stress(zooms):
-    test_zoom, target_zoom = zooms
+def test_stress():
+    """ Make sure that out zoom is consistent with scipy's """
     for i in range(32):
         shape = np.random.randint(64, 128, size=np.random.randint(1, 4))
         inp = np.random.randn(*shape)
-        scale = np.random.uniform(0.5, 2, size=1 if np.random.binomial(1, 0.5) else inp.ndim)
-        if len(scale) == 1:
-            scale = scale[0]
-        axis = {'axis': np.arange(len(inp.shape))} if test_zoom == zoom else {}
+        scale = np.random.uniform(0.5, 2, size=inp.ndim)
 
         without_borders = np.index_exp[:-1, :-1, :-1][: inp.ndim]
 
         # rtol=1e-6 as there is still some inconsistency
         allclose(
-            test_zoom(inp, scale, **axis)[without_borders],
-            target_zoom(inp, scale, order=1, **axis)[without_borders],
+            zoom(inp, scale)[without_borders],
+            scipy_zoom(inp, scale, order=1)[without_borders],
             rtol=1e-6,
             err_msg=f'{i, shape, scale}',
         )
