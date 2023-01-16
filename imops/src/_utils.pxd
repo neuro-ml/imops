2758f8b95a447b55542a8714f5b900c19f37a419
Philipenko Vladimir <vovaf709@yandex.ru>
2023-01-16 18:55:29 +0300

    Revert "Move strides upper"
    This reverts commit 806bf53e39b8b96e13dc3a08b87d912b84168db1.

--- imops/src/_utils.pxd
+++ imops/src/_utils.pxd
@@ -14,13 +14,11 @@ ctypedef fused BOOL_FP32_FP64:
 # FIXME: Why cython throws warning on inline modifier in .pxd?
 cdef inline BOOL_FP32_FP64 get_pixel2d(BOOL_FP32_FP64* input,
                                        Py_ssize_t rows, Py_ssize_t cols,
-                                       Py_ssize_t stride_r, Py_ssize_t stride_c,
                                        Py_ssize_t r, Py_ssize_t c,
                                        BOOL_FP32_FP64 cval) nogil
 
 
 cdef inline BOOL_FP32_FP64 get_pixel3d(BOOL_FP32_FP64* input,
                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                       Py_ssize_t stride_r, Py_ssize_t stride_c, Py_ssize_t stride_d,
                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
                                        BOOL_FP32_FP64 cval) nogil
--- imops/src/_utils.pyx
+++ imops/src/_utils.pyx
@@ -11,21 +11,19 @@ cimport numpy as np
 
 cdef inline BOOL_FP32_FP64 get_pixel2d(BOOL_FP32_FP64* input,
                                        Py_ssize_t rows, Py_ssize_t cols,
-                                       Py_ssize_t stride_r, Py_ssize_t stride_c,
                                        Py_ssize_t r, Py_ssize_t c,
                                        BOOL_FP32_FP64 cval) nogil:
     if (r < 0) or (r >= rows) or (c < 0) or (c >= cols):
         return cval
 
-    return input[r * stride_r + c * stride_c]
+    return input[r * cols + c]
 
 
 cdef inline BOOL_FP32_FP64 get_pixel3d(BOOL_FP32_FP64* input,
                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                       Py_ssize_t stride_r, Py_ssize_t stride_c, Py_ssize_t stride_d,
                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
                                        BOOL_FP32_FP64 cval) nogil:
     if (r < 0) or (r >= rows) or (c < 0) or (c >= cols) or (d < 0) or (d >= dims):
         return cval
 
-    return input[r * stride_r + c * stride_c + d * stride_d]
+    return input[r * cols * dims + c * dims + d]
