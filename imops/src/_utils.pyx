5b84a1af2b642403af511387dfa10e37e22ce1de
Philipenko Vladimir <vovaf709@yandex.ru>
2023-01-16 18:33:32 +0300

    remove trash

--- imops/src/_utils.pyx
+++ imops/src/_utils.pyx
@@ -27,5 +27,5 @@ cdef inline BOOL_FP32_FP64 get_pixel3d(BOOL_FP32_FP64* input,
                                        BOOL_FP32_FP64 cval) nogil:
     if (r < 0) or (r >= rows) or (c < 0) or (c >= cols) or (d < 0) or (d >= dims):
         return cval
-    # cols * dims, dims, 1 
+
     return input[r * stride_r + c * stride_c + d * stride_d]
