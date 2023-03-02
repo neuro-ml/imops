cdaa936ac2d0d482f5d6fc2a36151705394f6b2e
Philipenko Vladimir <vovaf709@yandex.ru>
2023-03-02 13:23:21 +0300

    Benchmark for `center_of_mass`

--- benchmarks/benchmark_measure.py
+++ benchmarks/benchmark_measure.py
@@ -1,7 +1,10 @@
 import numpy as np
 from skimage.measure import label as sk_label
 
-from imops.measure import label
+from imops._configs import numeric_configs
+from imops.measure import center_of_mass, label
+
+from .common import discard_arg
 
 
 class LabelSuite:
@@ -19,3 +22,20 @@ class LabelSuite:
 
     def peakmem_sk_label(self):
         sk_label(self.image)
+
+
+class CenterOfMassSuite:
+    params = [numeric_configs, ('float32', 'float64')]
+    param_names = ['backend', 'dtype']
+
+    @discard_arg(1)
+    def setup(self, dtype):
+        self.image = np.random.randn(512, 512, 512).astype(dtype)
+
+    @discard_arg(2)
+    def time_center_of_mass(self, backend):
+        center_of_mass(self.image, backend=backend)
+
+    @discard_arg(2)
+    def peakmem_center_of_mass(self, backend):
+        center_of_mass(self.image, backend=backend)
