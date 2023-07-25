600646de246d1c61539f85d1420e4d620a5c0220
LeoQay <reliativism@yandex.ru>
2023-07-26 01:59:20 +0300

    Fix bug with input.shape[i] < footprint.shape[i] for some i

--- imops/src/_morphology.pyx
+++ imops/src/_morphology.pyx
@@ -13,147 +13,6 @@ from cython.parallel import prange
 from libc.stdlib cimport abort, malloc, free
 
 
-# TODO: Move generic functions like this to the separate file
-cdef inline np.uint8_t get_pixel3d(np.uint8_t* input,
-                                   Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                   Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                   np.uint8_t cval, layout = b'C') nogil:
-    if (r < 0) or (r >= rows) or (c < 0) or (c >= cols) or (d < 0) or (d >= dims):
-        return cval
-
-    if layout == b'C':
-        return input[r * cols * dims + c * dims + d]
-
-    if layout == b'F':
-        return input[rows * cols * d + rows * c + r]
-
-
-cdef inline np.uint8_t max_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
-                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
-    cdef Py_ssize_t i, j, k
-    cdef Py_ssize_t i_r, i_c, i_d
-
-    cdef int rows_shift = r + f_rows // 2
-    cdef int cols_shift = c + f_cols // 2
-    cdef int dims_shift = d + f_dims // 2
-
-    for i in range(f_rows):
-        i_r = rows_shift - i
-        for j in range(f_cols):
-            i_c = cols_shift - j
-            for k in range(f_dims):
-                i_d = dims_shift - k
-
-                if (
-                    get_pixel3d(
-                        footprint,
-                        f_rows, f_cols, f_dims,
-                        i, j, k,
-                        False,
-                    ) and
-                    get_pixel3d(
-                        input,
-                        rows, cols, dims,
-                        i_r, i_c, i_d,
-                        False,
-                    )
-                ):
-                    return True
-
-    return False
-
-
-cdef inline np.uint8_t min_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
-                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
-    cdef Py_ssize_t i, j, k
-    cdef Py_ssize_t i_r, i_c, i_d
-
-    cdef int rows_shift = r - f_rows // 2
-    cdef int cols_shift = c - f_cols // 2
-    cdef int dims_shift = d - f_dims // 2
-
-    for i in range(f_rows):
-        i_r = rows_shift + i
-        for j in range(f_cols):
-            i_c = cols_shift + j
-            for k in range(f_dims):
-                i_d = dims_shift + k
-
-                if (
-                    get_pixel3d(
-                        footprint,
-                        f_rows, f_cols, f_dims,
-                        i, j, k,
-                        False,
-                    ) and not
-                    get_pixel3d(
-                        input,
-                        rows, cols, dims,
-                        i_r, i_c, i_d,
-                        True,
-                    )
-                ):
-                    return False
-
-    return True
-
-
-def _binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
-    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
-    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
-
-    cdef np.uint8_t[:, :, ::1] dilated = np.zeros_like(input, dtype=np.uint8)
-
-    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
-    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
-    cdef Py_ssize_t i, j, k
-
-    for i in prange(rows, nogil=True, num_threads=num_threads):
-        for j in prange(cols):
-            for k in prange(dims):
-                dilated[i, j, k] = max_in_footprint(
-                    &contiguous_input[0, 0, 0],
-                    &contiguous_footprint[0, 0, 0],
-                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
-                )
-
-    return np.asarray(dilated)
-
-
-def _binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
-    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
-    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
-
-    cdef np.uint8_t[:, :, ::1] eroded = np.zeros_like(input, dtype=np.uint8)
-
-    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
-    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
-    cdef Py_ssize_t i, j, k
-
-    for i in prange(rows, nogil=True, num_threads=num_threads):
-        for j in prange(cols):
-            for k in prange(dims):
-                eroded[i, j, k] = min_in_footprint(
-                    &contiguous_input[0, 0, 0],
-                    &contiguous_footprint[0, 0, 0],
-                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
-                )
-
-    return np.asarray(eroded)
-
-
-# cython: boundscheck = False
-# cython: initializedcheck = False
-# cython: wraparound = False
-# cython: cdivision = True
-# cython: nonecheck = False
-# cython: language_level = 3
-
-
 cdef struct array_iterator:
     int rank_m1
     int dimensions[3]
@@ -303,16 +162,20 @@ cdef int init_filter_offsets(
     return 0
 
 
-cdef inline int filter_iterator_offset(filter_iterator *filter_iter, int *coordinates, np.uint8_t is_dilation) nogil:
-    cdef int i, offset = 0
+cdef inline int filter_iterator_offset(filter_iterator *filter_iter, int *coordinates) nogil:
+    cdef int i, position, offset = 0
 
     for i in range(3):
-        if coordinates[i] < filter_iter[0].bound1[i]:
-            offset += filter_iter[0].strides[i] * coordinates[i]
+        if filter_iter[0].bound1[i] > filter_iter[0].bound2[i]:
+            position = coordinates[i]
+        elif coordinates[i] < filter_iter[0].bound1[i]:
+            position = coordinates[i]
         elif coordinates[i] >= filter_iter[0].bound2[i]:
-            offset += filter_iter[0].strides[i] * (filter_iter[0].bound1[i] + coordinates[i] - filter_iter[0].bound2[i])
+            position = filter_iter[0].bound1[i] + coordinates[i] - filter_iter[0].bound2[i]
         else:
-            offset += filter_iter[0].strides[i] * filter_iter[0].bound1[i]
+            position = filter_iter[0].bound1[i]
+        
+        offset += filter_iter[0].strides[i] * position
     
     return offset
 
@@ -343,7 +206,7 @@ cdef inline int worker(
 
     cdef np.uint8_t *pi = input + start
     cdef np.uint8_t *po = output + start
-    cdef int *oo = offsets + filter_iterator_offset(&filter_iter, <int*>input_iter.coordinates, is_dilation)
+    cdef int *oo = offsets + filter_iterator_offset(&filter_iter, <int*>input_iter.coordinates)
 
     cdef np.uint8_t out
     cdef int _oo, _pp
@@ -444,10 +307,145 @@ def _binary_operation(
     return np.asarray(output)
 
 
-def new_binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
+def _binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
     return _binary_operation(input, footprint, num_threads, True, False)
 
 
-def new_binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
+def _binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
     inverted_footprint = np.array(footprint[::-1, ::-1, ::-1])
     return _binary_operation(input, inverted_footprint, num_threads, False, True)
+
+
+
+
+# TODO: Move generic functions like this to the separate file
+cdef inline np.uint8_t get_pixel3d(np.uint8_t* input,
+                                   Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
+                                   Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
+                                   np.uint8_t cval, layout = b'C') nogil:
+    if (r < 0) or (r >= rows) or (c < 0) or (c >= cols) or (d < 0) or (d >= dims):
+        return cval
+
+    if layout == b'C':
+        return input[r * cols * dims + c * dims + d]
+
+    if layout == b'F':
+        return input[rows * cols * d + rows * c + r]
+
+
+cdef inline np.uint8_t max_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
+                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
+                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
+                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
+    cdef Py_ssize_t i, j, k
+    cdef Py_ssize_t i_r, i_c, i_d
+
+    cdef int rows_shift = r + f_rows // 2
+    cdef int cols_shift = c + f_cols // 2
+    cdef int dims_shift = d + f_dims // 2
+
+    for i in range(f_rows):
+        i_r = rows_shift - i
+        for j in range(f_cols):
+            i_c = cols_shift - j
+            for k in range(f_dims):
+                i_d = dims_shift - k
+
+                if (
+                    get_pixel3d(
+                        footprint,
+                        f_rows, f_cols, f_dims,
+                        i, j, k,
+                        False,
+                    ) and
+                    get_pixel3d(
+                        input,
+                        rows, cols, dims,
+                        i_r, i_c, i_d,
+                        False,
+                    )
+                ):
+                    return True
+
+    return False
+
+
+cdef inline np.uint8_t min_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
+                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
+                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
+                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
+    cdef Py_ssize_t i, j, k
+    cdef Py_ssize_t i_r, i_c, i_d
+
+    cdef int rows_shift = r - f_rows // 2
+    cdef int cols_shift = c - f_cols // 2
+    cdef int dims_shift = d - f_dims // 2
+
+    for i in range(f_rows):
+        i_r = rows_shift + i
+        for j in range(f_cols):
+            i_c = cols_shift + j
+            for k in range(f_dims):
+                i_d = dims_shift + k
+
+                if (
+                    get_pixel3d(
+                        footprint,
+                        f_rows, f_cols, f_dims,
+                        i, j, k,
+                        False,
+                    ) and not
+                    get_pixel3d(
+                        input,
+                        rows, cols, dims,
+                        i_r, i_c, i_d,
+                        True,
+                    )
+                ):
+                    return False
+
+    return True
+
+
+def old_binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
+    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
+    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
+
+    cdef np.uint8_t[:, :, ::1] dilated = np.zeros_like(input, dtype=np.uint8)
+
+    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
+    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
+    cdef Py_ssize_t i, j, k
+
+    for i in prange(rows, nogil=True, num_threads=num_threads):
+        for j in prange(cols):
+            for k in prange(dims):
+                dilated[i, j, k] = max_in_footprint(
+                    &contiguous_input[0, 0, 0],
+                    &contiguous_footprint[0, 0, 0],
+                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
+                )
+
+    return np.asarray(dilated)
+
+
+def old_binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
+    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
+    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
+
+    cdef np.uint8_t[:, :, ::1] eroded = np.zeros_like(input, dtype=np.uint8)
+
+    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
+    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
+    cdef Py_ssize_t i, j, k
+
+    for i in prange(rows, nogil=True, num_threads=num_threads):
+        for j in prange(cols):
+            for k in prange(dims):
+                eroded[i, j, k] = min_in_footprint(
+                    &contiguous_input[0, 0, 0],
+                    &contiguous_footprint[0, 0, 0],
+                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
+                )
+
+    return np.asarray(eroded)
