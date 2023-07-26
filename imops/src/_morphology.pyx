4e2b454670af86d8ea01386a849c401a438db566
LeoQay <reliativism@yandex.ru>
2023-07-26 19:50:02 +0300

    code cleaning in _morphology.pyx

--- imops/src/_morphology.pyx
+++ imops/src/_morphology.pyx
@@ -10,7 +10,8 @@ import numpy as np
 cimport numpy as np
 
 from cython.parallel import prange
-from libc.stdlib cimport abort, malloc, free
+
+from libc.stdlib cimport abort, free, malloc
 
 
 cdef struct array_iterator:
@@ -25,13 +26,13 @@ cdef int init_array_iterator(array_iterator *arr_iter, int *shape, int *strides)
     cdef int i
 
     arr_iter.rank_m1 = 2
-    
+
     for i in range(3):
         arr_iter[0].dimensions[i] = shape[i] - 1
         arr_iter[0].coordinates[i] = 0
         arr_iter[0].strides[i] = strides[i]
         arr_iter[0].backstrides[i] = strides[i] * arr_iter[0].dimensions[i]
-    
+
     return 0
 
 
@@ -50,12 +51,12 @@ cdef int init_filter_iterator(
     np.uint8_t is_dilation
 ) nogil:
     cdef int i, rank = 3, step, orgn
-    
+
     filter_iter[0].strides[rank - 1] = filter_size
     for i in range(rank - 2, -1, -1):
         step = a_shape[i + 1] if a_shape[i + 1] < f_shape[i + 1] else f_shape[i + 1]
         filter_iter[0].strides[i] = filter_iter[0].strides[i + 1] * step
-    
+
     for i in range(rank):
         step = a_shape[i] if a_shape[i] < f_shape[i] else f_shape[i]
         orgn = f_shape[i] // 2
@@ -65,10 +66,10 @@ cdef int init_filter_iterator(
         filter_iter[0].backstrides[i] = (step - 1) * filter_iter[0].strides[i]
         filter_iter[0].bound1[i] = orgn
         filter_iter[0].bound2[i] = a_shape[i] - f_shape[i] + orgn
-    
+
     return 0
 
-        
+
 cdef int init_filter_offsets(
     int *a_shape, int *a_strides,
     np.uint8_t *footprint, int *f_shape,
@@ -86,24 +87,26 @@ cdef int init_filter_offsets(
 
     for i in range(3):
         filter_size *= f_shape[i]
-    
+
     for i in range(3):
         offsets_size *= (a_shape[i] if a_shape[i] < f_shape[i] else f_shape[i])
-    
-    offsets[0] = <int*>malloc(offsets_size * footprint_size * sizeof(int));
+
+    offsets[0] = <int*>malloc(offsets_size * footprint_size * sizeof(int))
+    if offsets[0] == 0:
+        abort()
 
     for i in range(3):
         if a_shape[i] > max_size:
             max_size = a_shape[i]
-        
+
         stride = -a_strides[i] if a_strides[i] < 0 else a_strides[i]
 
         if stride > max_stride:
             max_stride = stride
-        
+
         coordinates[i] = 0
         position[i] = 0
-    
+
     border_flag_value[0] = max_size * max_stride + 1
     po = offsets[0]
 
@@ -127,11 +130,11 @@ cdef int init_filter_offsets(
                         # use an offset that is possibly mapped from outside the border:
                         cc -= position[i]
                         offset += a_strides[i] * cc
-            
+
                 # store the offset
                 po[0] = offset
                 po += 1
-        
+
             # next point in the filter:
             for i in range(3 - 1, -1, -1):
                 if coordinates[i] < f_shape[i] - 1:
@@ -139,12 +142,12 @@ cdef int init_filter_offsets(
                     break
                 else:
                     coordinates[i] = 0
-        
+
         # move to the next array region:
         for i in range(3 - 1, -1, -1):
             orgn = f_shape[i] // 2
             if is_dilation and f_shape[i] % 2 == 0:
-                        orgn -= 1
+                orgn -= 1
 
             if position[i] == orgn:
                 position[i] += a_shape[i] - f_shape[i] + 1
@@ -153,12 +156,12 @@ cdef int init_filter_offsets(
                     position[i] = orgn + 1
             else:
                 position[i] += 1
-            
+
             if position[i] < a_shape[i]:
                 break
             else:
                 position[i] = 0
-    
+
     return 0
 
 
@@ -174,9 +177,9 @@ cdef inline int filter_iterator_offset(filter_iterator *filter_iter, int *coordi
             position = filter_iter[0].bound1[i] + coordinates[i] - filter_iter[0].bound2[i]
         else:
             position = filter_iter[0].bound1[i]
-        
+
         offset += filter_iter[0].strides[i] * position
-    
+
     return offset
 
 
@@ -216,7 +219,7 @@ cdef inline int worker(
 
         for i in range(footprint_size):
             _oo = oo[i]
-        
+
             if _oo == border_flag_value:
                 if border_value == _false:
                     out = _false
@@ -234,17 +237,17 @@ cdef inline int worker(
             if _pp < input_iter.dimensions[i]:
                 if _pp < filter_iter.bound1[i] or _pp >= filter_iter.bound2[i]:
                     oo += filter_iter.strides[i]
-                
+
                 input_iter.coordinates[i] += 1
                 break
             else:
                 input_iter.coordinates[i] = 0
 
                 oo -= filter_iter.backstrides[i]
-        
+
         pi += 1
         po += 1
-    
+
     return 0
 
 
@@ -254,7 +257,7 @@ def _binary_operation(
     Py_ssize_t num_threads,
     np.uint8_t border_value,
     np.uint8_t is_dilation
-    ):
+):
     cdef np.uint8_t[:, :, ::1] c_input = np.ascontiguousarray(input)
     cdef np.uint8_t[:, :, ::1] c_footprint = np.ascontiguousarray(footprint)
     cdef np.uint8_t[:, :, ::1] output = np.zeros_like(input, dtype=np.uint8)
@@ -281,7 +284,7 @@ def _binary_operation(
     )
 
     cdef int task, start, chunk
-    
+
     cdef int _mod = size % num_threads, _div = size // num_threads
 
     for task in prange(num_threads, nogil=True, num_threads=num_threads):
@@ -291,7 +294,7 @@ def _binary_operation(
         else:
             chunk = _div
             start = chunk * task + _mod
-        
+
         worker(
             &c_input[0, 0, 0], &c_footprint[0, 0, 0], &output[0, 0, 0],
             a_shape, a_strides, f_shape,
@@ -301,7 +304,7 @@ def _binary_operation(
             border_value,
             is_dilation
         )
-    
+
     free(offsets)
 
     return np.asarray(output)
@@ -314,138 +317,3 @@ def _binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py
 def _binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
     inverted_footprint = np.array(footprint[::-1, ::-1, ::-1])
     return _binary_operation(input, inverted_footprint, num_threads, False, True)
-
-
-
-
-# TODO: Move generic functions like this to the separate file
-cdef inline np.uint8_t get_pixel3d(np.uint8_t* input,
-                                   Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                   Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                   np.uint8_t cval, layout = b'C') nogil:
-    if (r < 0) or (r >= rows) or (c < 0) or (c >= cols) or (d < 0) or (d >= dims):
-        return cval
-
-    if layout == b'C':
-        return input[r * cols * dims + c * dims + d]
-
-    if layout == b'F':
-        return input[rows * cols * d + rows * c + r]
-
-
-cdef inline np.uint8_t max_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
-                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
-    cdef Py_ssize_t i, j, k
-    cdef Py_ssize_t i_r, i_c, i_d
-
-    cdef int rows_shift = r + f_rows // 2
-    cdef int cols_shift = c + f_cols // 2
-    cdef int dims_shift = d + f_dims // 2
-
-    for i in range(f_rows):
-        i_r = rows_shift - i
-        for j in range(f_cols):
-            i_c = cols_shift - j
-            for k in range(f_dims):
-                i_d = dims_shift - k
-
-                if (
-                    get_pixel3d(
-                        footprint,
-                        f_rows, f_cols, f_dims,
-                        i, j, k,
-                        False,
-                    ) and
-                    get_pixel3d(
-                        input,
-                        rows, cols, dims,
-                        i_r, i_c, i_d,
-                        False,
-                    )
-                ):
-                    return True
-
-    return False
-
-
-cdef inline np.uint8_t min_in_footprint(np.uint8_t* input, np.uint8_t* footprint,
-                                        Py_ssize_t r, Py_ssize_t c, Py_ssize_t d,
-                                        Py_ssize_t rows, Py_ssize_t cols, Py_ssize_t dims,
-                                        Py_ssize_t f_rows, Py_ssize_t f_cols, Py_ssize_t f_dims) nogil:
-    cdef Py_ssize_t i, j, k
-    cdef Py_ssize_t i_r, i_c, i_d
-
-    cdef int rows_shift = r - f_rows // 2
-    cdef int cols_shift = c - f_cols // 2
-    cdef int dims_shift = d - f_dims // 2
-
-    for i in range(f_rows):
-        i_r = rows_shift + i
-        for j in range(f_cols):
-            i_c = cols_shift + j
-            for k in range(f_dims):
-                i_d = dims_shift + k
-
-                if (
-                    get_pixel3d(
-                        footprint,
-                        f_rows, f_cols, f_dims,
-                        i, j, k,
-                        False,
-                    ) and not
-                    get_pixel3d(
-                        input,
-                        rows, cols, dims,
-                        i_r, i_c, i_d,
-                        True,
-                    )
-                ):
-                    return False
-
-    return True
-
-
-def old_binary_dilation(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
-    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
-    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
-
-    cdef np.uint8_t[:, :, ::1] dilated = np.zeros_like(input, dtype=np.uint8)
-
-    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
-    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
-    cdef Py_ssize_t i, j, k
-
-    for i in prange(rows, nogil=True, num_threads=num_threads):
-        for j in prange(cols):
-            for k in prange(dims):
-                dilated[i, j, k] = max_in_footprint(
-                    &contiguous_input[0, 0, 0],
-                    &contiguous_footprint[0, 0, 0],
-                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
-                )
-
-    return np.asarray(dilated)
-
-
-def old_binary_erosion(np.uint8_t[:, :, :] input, np.uint8_t[:, :, :] footprint, Py_ssize_t num_threads):
-    cdef np.uint8_t[:, :, ::1] contiguous_input = np.ascontiguousarray(input)
-    cdef np.uint8_t[:, :, ::1] contiguous_footprint = np.ascontiguousarray(footprint)
-
-    cdef np.uint8_t[:, :, ::1] eroded = np.zeros_like(input, dtype=np.uint8)
-
-    cdef Py_ssize_t rows = input.shape[0], cols = input.shape[1], dims = input.shape[2]
-    cdef Py_ssize_t f_rows = footprint.shape[0], f_cols = footprint.shape[1], f_dims = footprint.shape[2]
-    cdef Py_ssize_t i, j, k
-
-    for i in prange(rows, nogil=True, num_threads=num_threads):
-        for j in prange(cols):
-            for k in prange(dims):
-                eroded[i, j, k] = min_in_footprint(
-                    &contiguous_input[0, 0, 0],
-                    &contiguous_footprint[0, 0, 0],
-                    i, j, k, rows, cols, dims, f_rows, f_cols, f_dims
-                )
-
-    return np.asarray(eroded)
