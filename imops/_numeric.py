3848874e13b6cc78362f57bbf8d69f8e979f5ec2
Philipenko Vladimir <vovaf709@yandex.ru>
2023-09-05 13:11:07 +0300

    Remove `_mul` & `_sum`

--- imops/_numeric.py
+++ imops/_numeric.py
@@ -1,5 +1,4 @@
 from typing import Callable, Union
-from warnings import warn
 
 import numpy as np
 
@@ -9,132 +8,17 @@ from .src._fast_numeric import (
     _pointwise_add_array_4d as cython_fast_pointwise_add_array_4d,
     _pointwise_add_value_3d as cython_fast_pointwise_add_value_3d,
     _pointwise_add_value_4d as cython_fast_pointwise_add_value_4d,
-    _pointwise_mul_3d as cython_fast_pointwise_mul_3d,
-    _sum_1d as cython_fast_sum_1d,
 )
 from .src._numeric import (
     _pointwise_add_array_3d as cython_pointwise_add_array_3d,
     _pointwise_add_array_4d as cython_pointwise_add_array_4d,
     _pointwise_add_value_3d as cython_pointwise_add_value_3d,
     _pointwise_add_value_4d as cython_pointwise_add_value_4d,
-    _pointwise_mul_3d as cython_pointwise_mul_3d,
-    _sum_1d as cython_sum_1d,
 )
 from .utils import normalize_num_threads
 
 
 TYPES = (np.int16, np.int32, np.int64, np.float32, np.float64)
-
-
-def _sum(nums: np.ndarray, num_threads: int = -1, backend: BackendLike = None) -> float:
-    """
-    Parallel sum of flat numpy array
-
-    Parameters
-    ----------
-    nums: np.ndarray
-        1-dimensional array
-    num_threads: int
-        the number of threads to use for computation. Default = the cpu count. If negative value passed
-        cpu count + num_threads + 1 threads will be used
-    backend: BackendLike
-        which backend to use. `cython` and `scipy` (primarly for benchmarking) are available,
-        `cython` is used by default
-
-    Returns
-    -------
-    sum: float
-
-    Examples
-    --------
-    >>> s = _sum(x, num_threads=1)
-    >>> s = _sum(x, num_threads=8, backend=Cython(fast=True))  # ffast-math compiled version
-    """
-    ndim = nums.ndim
-
-    if ndim != 1:
-        raise ValueError(f'Input must be 1-dimensional instead of {ndim}-dimensional.')
-
-    backend = resolve_backend(backend)
-    if backend.name not in ('Cython', 'Scipy'):
-        raise ValueError(f'Unsupported backend "{backend.name}"')
-
-    num_threads = normalize_num_threads(num_threads, backend)
-
-    if backend.name == 'Scipy':
-        return nums.sum()
-
-    if backend.name == 'Cython':
-        src_sum_1d = cython_fast_sum_1d if backend.fast else cython_sum_1d
-
-    return src_sum_1d(nums, num_threads)
-
-
-def _mul(nums1: np.ndarray, nums2: np.ndarray, num_threads: int = -1, backend: BackendLike = None) -> np.ndarray:
-    """
-    Parallel pointwise multiplication of 2 numpy arrays (aka x * y). Works faster only for ndim <= 3.
-
-    Parameters
-    ----------
-    nums1: np.ndarray
-    nums2: np.ndarray
-    num_threads: int
-        the number of threads to use for computation. Default = the cpu count. If negative value passed
-        cpu count + num_threads + 1 threads will be used
-    backend: BackendLike
-        which backend to use. `cython` and `scipy` (primarly for benchmarking) are available,
-        `cython` is used by default
-
-    Returns
-    -------
-    multiplied: np.ndarray
-        result of nums1 * nums2
-
-    Examples
-    --------
-    >>> mul = _mul(nums1, nums2, num_threads=8)
-    >>> mul = _mul(np.ones((2, 3)), np.ones((1, 3)))  # broadcasting, mul.shape == (2, 3)
-    >>> mul = _mul(nums1, nums2, backend=Cython(fast=True))  # ffast-math compiled version
-    """
-    if not nums1.size and not nums2.size:
-        return np.array([], dtype=nums1.dtype)
-    if bool(nums1.size) ^ bool(nums2.size):
-        raise ValueError('One of the arrays is empty, hence pointwise multiplication cannot be performed.')
-    if len(nums1.shape) != len(nums2.shape):
-        raise ValueError('Both arrays must have the same number of dimensions for pointwise multiplication.')
-    for dim1, dim2 in zip(nums1.shape, nums2.shape):
-        if dim1 != dim2 and dim1 != 1 and dim2 != 1:
-            raise ValueError(f'Arrays of shapes {nums1.shape} and {nums2.shape} are not broadcastable.')
-
-    if nums1.ndim > 3:
-        warn('Parallel pointwise multiplication is only supported for ndim<=3. Falling back to naive x * y.')
-
-        return nums1 * nums2
-
-    backend = resolve_backend(backend)
-    if backend.name not in ('Cython', 'Scipy'):
-        raise ValueError(f'Unsupported backend "{backend.name}"')
-
-    num_threads = normalize_num_threads(num_threads, backend)
-
-    if backend.name == 'Scipy':
-        return nums1 * nums2
-
-    if backend.name == 'Cython':
-        src_pointwise_mul_3d = cython_fast_pointwise_mul_3d if backend.fast else cython_pointwise_mul_3d
-
-    n_dummy = 3 - nums1.ndim
-
-    if n_dummy:
-        nums1 = nums1[(None,) * n_dummy]
-        nums2 = nums2[(None,) * n_dummy]
-
-    out = src_pointwise_mul_3d(nums1, nums2, np.maximum(nums1.shape, nums2.shape), num_threads)
-
-    if n_dummy:
-        out = out[(0,) * n_dummy]
-
-    return out
 
 
 def _choose_cython_pointwise_add(ndim: int, summand_is_array: bool, fast: bool) -> Callable:
--- imops/src/_numeric.pyx
+++ imops/src/_numeric.pyx
@@ -20,51 +20,7 @@ ctypedef fused NUM:
     double
 
 
-def _sum_1d(NUM[:] nums, Py_ssize_t num_threads) -> NUM:
-    cdef NUM res = 0
-    cdef Py_ssize_t i, len_nums = len(nums)
-
-    for i in prange(len_nums, num_threads=num_threads, nogil=True):
-        res += nums[i]
-
-    return res
-
-
 # TODO: Generalize code below to n-d
-def _pointwise_mul_3d(
-    NUM[:, :, :] nums1,
-    NUM[:, :, :] nums2,
-    Py_ssize_t[:] res_shape,
-    Py_ssize_t num_threads,
-) -> np.ndarray:
-    cdef NUM[:, :, ::1] contiguous_nums1 = np.ascontiguousarray(nums1), contiguous_nums2 = np.ascontiguousarray(nums2)
-    cdef Py_ssize_t rows = res_shape[0], cols = res_shape[1], dims = res_shape[2]
-
-    cdef char[:] broadcast_mask1 = np.array([x == y for x, y in zip(res_shape, nums1.shape)], dtype=np.int8)
-    cdef char[:] broadcast_mask2 = np.array([x == y for x, y in zip(res_shape, nums2.shape)], dtype=np.int8)
-
-    cdef NUM[:, :, ::1] mul = np.empty_like(nums1, shape=res_shape)
-    cdef Py_ssize_t i, j, k
-
-    for i in prange(rows, nogil=True, num_threads=num_threads):
-        for j in prange(cols):
-            for k in prange(dims):
-                mul[i, j, k] = (
-                    contiguous_nums1[
-                        i * broadcast_mask1[0],
-                        j * broadcast_mask1[1],
-                        k * broadcast_mask1[2]
-                    ] *
-                    contiguous_nums2[
-                        i * broadcast_mask2[0],
-                        j * broadcast_mask2[1],
-                        k * broadcast_mask2[2]
-                    ]
-                )
-
-    return np.asarray(mul)
-
-
 def _pointwise_add_array_3d(
     NUM[:, :, :] nums1,
     NUM[:, :, :] nums2,
