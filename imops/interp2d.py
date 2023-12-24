069e164ba56a1cb96e48c8f43a89c0b5f0ead2e9
Philipenko Vladimir <vovaf709@yandex.ru>
2023-12-24 11:04:45 +0300

    Compressed test data, ablate `n_jobs` in tests

--- imops/interp2d.py
+++ imops/interp2d.py
@@ -8,29 +8,33 @@ from cpp_modules import Linear2DInterpolatorCpp
 
 class Linear2DInterpolator(Linear2DInterpolatorCpp):
     # """
-    # #### 2D delaunay triangulation and parallel linear interpolation     
-    # Example:      
-    # n, m = 1024, 2        
-    # points = np.random.randint(low=0, high=1024, size=(n, m))        
-    # points = np.unique(points, axis=0)        
-    # x_points = points[: n // 2]        
-    # values = np.random.uniform(low=0.0, high=1.0, size=(len(x_points),))        
-    # interp_points = points[n // 2:]        
-    # n_jobs = -1 // will be equal to num of CPU cores        
-    # interpolator = Linear2DInterpolator(x_points, values, n_jobs)        
-    # // Also you can pass values to __call__ and rewrite the ones that were passed to __init__        
+    # #### 2D delaunay triangulation and parallel linear interpolation
+    # Example:
+    # n, m = 1024, 2
+    # points = np.random.randint(low=0, high=1024, size=(n, m))
+    # points = np.unique(points, axis=0)
+    # x_points = points[: n // 2]
+    # values = np.random.uniform(low=0.0, high=1.0, size=(len(x_points),))
+    # interp_points = points[n // 2:]
+    # n_jobs = -1 // will be equal to num of CPU cores
+    # interpolator = Linear2DInterpolator(x_points, values, n_jobs)
+    # // Also you can pass values to __call__ and rewrite the ones that were passed to __init__
     # interp_values = interpolator(interp_points, values + 1.0, fill_value=0.0)
     # """
 
-    def __init__(self, points: np.ndarray, values: np.ndarray, n_jobs: int = 1,  triangles: Optional[np.ndarray] = None, **kwargs):
+    def __init__(
+        self, points: np.ndarray, values: np.ndarray, n_jobs: int = 1, triangles: Optional[np.ndarray] = None, **kwargs
+    ):
 
         if triangles is not None:
             if isinstance(triangles, np.ndarray):
                 if triangles.ndim != 2 or triangles.shape[1] != 3 or triangles.shape[0] * 3 != triangles.size:
                     raise ValueError('Passed \"triangles\" argument has an incorrect shape')
             else:
-                raise ValueError(f'Wrong type of \"triangles\" argument, expected Optional[np.ndarray]. Got {type(triangles)}')
-            
+                raise ValueError(
+                    f'Wrong type of \"triangles\" argument, expected Optional[np.ndarray]. Got {type(triangles)}'
+                )
+
         if n_jobs <= 0 and n_jobs != -1:
             raise ValueError('Invalid number of workers, has to be -1 or positive integer')
 
@@ -39,14 +43,18 @@ class Linear2DInterpolator(Linear2DInterpolatorCpp):
         self.values = values
         self.n_jobs = n_jobs
 
-    def __call__(self, points: np.ndarray, values: Optional[np.ndarray] = None, fill_value: float = 0.0) -> np.ndarray[np.float32]:
-        
+    def __call__(
+        self, points: np.ndarray, values: Optional[np.ndarray] = None, fill_value: float = 0.0
+    ) -> np.ndarray[np.float32]:
+
         if values is not None:
-            if isinstance(values, np.ndarray):   
+            if isinstance(values, np.ndarray):
                 if values.ndim > 1:
                     raise ValueError(f'Wrong shape of \"values\" argument, expected ndim=1. Got shape {values.shape}')
             else:
-                 raise ValueError(f'Wrong type of \"values\" argument, expected Optional[np.ndarray]. Got {type(values)}')
+                raise ValueError(
+                    f'Wrong type of \"values\" argument, expected Optional[np.ndarray]. Got {type(values)}'
+                )
 
         self.values = self.values if values is None else values
 
@@ -57,6 +65,6 @@ class Linear2DInterpolator(Linear2DInterpolatorCpp):
 
         if not isinstance(points, np.ndarray):
             raise ValueError(f'Wrong type of \"points\" argument, expected np.ndarray. Got {type(points)}')
-        
+
         int_values = super().__call__(points, self.values, neighbors, fill_value)
         return int_values
--- tests/test_interp2d.py
+++ tests/test_interp2d.py
@@ -1,42 +1,58 @@
+from pathlib import Path
+
 import numpy as np
+import pytest
 import scipy
+from deli import load
 from scipy.interpolate import griddata
 
 from imops.interp2d import Linear2DInterpolator
-from deli import load_numpy
+
 
 np.random.seed(1337)
-n_jobs = 1
 
-def test_test_data():
+
+@pytest.fixture(params=range(1, 5))
+def n_jobs(request):
+    return request.param
+
+
+def test_test_data(n_jobs):
+    tests_root = Path(__file__).parent
     for i in range(2):
-        x = load_numpy(f'test_data/arr_{i}.npy')
+        x = load(tests_root / 'test_data' / f'arr_{i}.npy.gz')
         int_points = np.transpose((np.isnan(x)).nonzero())
         x_points: np.ndarray = np.transpose((~np.isnan(x)).nonzero())
         x_values = x[~np.isnan(x)]
         imops_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs)(int_points, fill_value=0.0)
         triangles = scipy.spatial.Delaunay(x_points).simplices
-        delaunay_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs, triangles=triangles)(int_points, fill_value=0.0)
+        delaunay_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs, triangles=triangles)(
+            int_points, fill_value=0.0
+        )
         scipy_values = griddata(x_points, x_values, int_points, method='linear', fill_value=0.0)
 
         delta_ds = np.abs(delaunay_values - scipy_values)
         # delta_di = np.abs(delaunay_values - imops_values)
         delta_si = np.abs(scipy_values - imops_values)
-        
-        assert delta_ds.max() <= 1e-10 and delta_si.max() <= 5, 'Failed with big cases, arr_0 or arr_1'
-    
+
+        assert delta_ds.max() <= 1e-10 and delta_si.max() <= 5, f'Failed with big case, arr_{i}'
+
     for i in range(2, 7):
-        x = load_numpy(f'test_data/arr_{i}.npy')
-        x_values = load_numpy(f'test_data/val_{i}.npy')
+        x = load(tests_root / 'test_data' / f'arr_{i}.npy.gz')
+        x_values = load(tests_root / 'test_data' / f'val_{i}.npy.gz')
         x_points = np.transpose(x.nonzero())
         int_points = np.transpose((~x).nonzero())
         imops_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs)(int_points, fill_value=0.0)
         triangles = scipy.spatial.Delaunay(x_points).simplices
-        delaunay_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs, triangles=triangles)(int_points, fill_value=0.0)
+        delaunay_values = Linear2DInterpolator(x_points, x_values, n_jobs=n_jobs, triangles=triangles)(
+            int_points, fill_value=0.0
+        )
         scipy_values = griddata(x_points, x_values, int_points, method='linear', fill_value=0.0)
 
         delta_ds = np.abs(delaunay_values - scipy_values)
         delta_di = np.abs(delaunay_values - imops_values)
         delta_si = np.abs(scipy_values - imops_values)
 
-        assert delta_di.max() <= 1.5 and delta_ds.max() <= 1e-10 and delta_si.max() <= 1.5, 'Failed with small cases, arr_2 ...'
+        assert (
+            delta_di.max() <= 1.5 and delta_ds.max() <= 1e-10 and delta_si.max() <= 1.5
+        ), f'Failed with small case: arr_{i}'
