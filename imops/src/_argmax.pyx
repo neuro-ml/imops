4837074303acce4057f6a1849e38f7905afc7c9c
AnihilatorGun <nikitaushakov.nu35@gmail.com>
2025-01-30 03:06:56 +0300

    fix argmax, build, authors:)

--- imops/src/_argmax.pyx
+++ imops/src/_argmax.pyx
@@ -6,20 +6,19 @@ import numpy as np
 
 cimport numpy as cnp
 from cython.parallel import prange
-from libc.math cimport ceilf, floorf
 
 cnp.import_array()
 
 
-def _first_index_4d_argmax(const FLOAT[:,:,:,:] x, Py_ssize_t C, Py_ssize_t I, Py_ssize_t J, Py_ssize_t K, Py_ssize_t num_threads):
+def _first_index_4d_argmax(const float[:,:,:,:] x, Py_ssize_t C, Py_ssize_t I, Py_ssize_t J, Py_ssize_t K, Py_ssize_t num_threads):
     cdef Py_ssize_t c, i, j, k
-    cdef FLOAT tmp
+    cdef float tmp
 
     cdef cnp.ndarray[dtype=cnp.uint8_t, ndim=3, mode="c"] out = np.zeros((I, J, K), dtype=np.uint8)
-    cdef cnp.ndarray[dtype=cnp.float32, ndim=3, mode="c"] tmp_max = np.copy(x[0])
+    cdef float[:, :, :] tmp_max = np.copy(x[0, :, :, :])
 
-    for c in prange(1, C, nogil=True, num_threads=num_threads)
-        for i in prange(0, I):
+    for c in range(1, C):
+        for i in prange(0, I, nogil=True, num_threads=num_threads):
             for j in prange(0, J):
                 for k in prange(0, K):
                     tmp = x[c, i, j, k]
