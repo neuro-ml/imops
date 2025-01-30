0912a6d0ff5722f13a4adc0dd8cad7f0bb15a96a
AnihilatorGun <nikitaushakov.nu35@gmail.com>
2025-01-30 16:26:03 +0300

    argmax for different positions

--- imops/src/_argmax.pyx
+++ imops/src/_argmax.pyx
@@ -10,21 +10,68 @@ from cython.parallel import prange
 cnp.import_array()
 
 
-def _first_index_4d_argmax(const float[:,:,:,:] x, Py_ssize_t C, Py_ssize_t I, Py_ssize_t J, Py_ssize_t K, Py_ssize_t num_threads):
-    cdef Py_ssize_t c, i, j, k
-    cdef float tmp
+def _inner_argmax(const float[:, :] x, Py_ssize_t C, Py_ssize_t I, Py_ssize_t num_threads):
+    cdef float tmp, tmp_max
+    cdef unsigned char cmax
+    cdef Py_ssize_t i, c
+    cdef cnp.ndarray[dtype=cnp.uint8_t, ndim=1, mode="c"] out = np.empty((I, ), dtype=np.uint8)
 
-    cdef cnp.ndarray[dtype=cnp.uint8_t, ndim=3, mode="c"] out = np.zeros((I, J, K), dtype=np.uint8)
-    cdef float[:, :, :] tmp_max = np.copy(x[0, :, :, :])
+    for i in prange(0, I, nogil=True, num_threads=num_threads):
+        tmp_max = x[0, i]
+        cmax = 0
 
-    for c in range(1, C):
-        for i in prange(0, I, nogil=True, num_threads=num_threads):
-            for j in prange(0, J):
-                for k in prange(0, K):
-                    tmp = x[c, i, j, k]
+        for c in range(1, C):
+            tmp = x[c, i]
 
-                    if tmp > tmp_max[i, j, k]:
-                        out[i, j, k] = c
-                        tmp_max[i, j, k] = tmp
+            if tmp > tmp_max:
+                cmax = c
+                tmp_max = tmp
+
+        out[i] = cmax
+
+    return out
+
+
+def _outer_argmax(const float[:, :] x, Py_ssize_t C, Py_ssize_t I, Py_ssize_t num_threads):
+    cdef float tmp, tmp_max
+    cdef unsigned char cmax
+    cdef Py_ssize_t i, c
+    cdef cnp.ndarray[dtype=cnp.uint8_t, ndim=1, mode="c"] out = np.empty((I, ), dtype=np.uint8)
+
+    for i in prange(0, I, nogil=True, num_threads=num_threads):
+        tmp_max = x[i, 0]
+        cmax = 0
+
+        for c in range(1, C):
+            tmp = x[i, c]
+
+            if tmp > tmp_max:
+                cmax = c
+                tmp_max = tmp
+
+        out[i] = cmax
+
+    return out
+
+
+def _outer_inner_argmax(const float[:, :, :] x, Py_ssize_t I, Py_ssize_t C, Py_ssize_t J, Py_ssize_t num_threads):
+    cdef float tmp, tmp_max
+    cdef unsigned char cmax
+    cdef Py_ssize_t i, c, j
+    cdef cnp.ndarray[dtype=cnp.uint8_t, ndim=2, mode="c"] out = np.empty((I, J), dtype=np.uint8)
+
+    for i in prange(0, I, nogil=True, num_threads=num_threads):
+        for j in prange(0, J):
+            tmp_max = x[i, 0, j]
+            cmax = 0
+
+            for c in range(1, C):
+                tmp = x[i, c, j]
+
+                if tmp > tmp_max:
+                    cmax = c
+                    tmp_max = tmp
+
+            out[i, j] = cmax
 
     return out
