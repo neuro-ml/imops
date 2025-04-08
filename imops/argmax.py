df66092b5b97b00e8decb62adb4ef1f77ce55bea
AnihilatorGun <nikitaushakov.nu35@gmail.com>
2025-04-09 00:46:29 +0300

    Revert "go back :despair:"
    This reverts commit d4cb66c2856a5ff315719c98ebdfa780830f9998.

--- imops/argmax.py
+++ imops/argmax.py
@@ -33,9 +33,19 @@ def argmax(array: np.ndarray, axis: AxesLike, num_threads: int = -1):
     result = argmax(x, axis=-1)
     ```
     """
-    if array.shape[axis] > 256 or array.dtype != np.float32:
+    if array.shape[axis] > 256 or array.dtype not in (
+        np.float32,
+        np.float64,
+        np.int16,
+        np.int32,
+        np.int64,
+        np.uint8,
+        np.uint16,
+        np.uint32,
+    ):
         warn(
-            'Fast argmax is only supported for array.shape[axis] <= 256, dtype=fp32'
+            'Fast argmax is only supported for array.shape[axis] <= 256, '
+            'dtype=fp32-fp64-int16-int32-int64-uint8-uint16-uint32. '
             "Falling back to numpy's implementation.",
             stacklevel=3,
         )
